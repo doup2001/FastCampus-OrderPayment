332de4707ca6a1e65d44b2c5a05d175041432296
eedo_y <doup2001@naver.com>
2025-01-28 00:11:46 +0900

    ✨feat : TossPayment 구현 - 인터페이스 기능 구현

--- src/main/java/lecture/fastcampus/ecommerce/fpay/infrastructure/out/pg/toss/TossPayments.java
+++ src/main/java/lecture/fastcampus/ecommerce/fpay/infrastructure/out/pg/toss/TossPayments.java
@@ -1,16 +1,22 @@
 package lecture.fastcampus.ecommerce.fpay.infrastructure.out.pg.toss;
 
+import lecture.fastcampus.ecommerce.fpay.application.port.out.api.PaymentAPIs;
 import lecture.fastcampus.ecommerce.fpay.infrastructure.out.pg.toss.response.ResponsePaymentApproved;
+import lecture.fastcampus.ecommerce.fpay.infrastructure.out.pg.toss.response.ResponsePaymentCancel;
+import lecture.fastcampus.ecommerce.fpay.infrastructure.out.pg.toss.response.ResponsePaymentSettlements;
 import lecture.fastcampus.ecommerce.fpay.representation.request.payment.PaymentApproved;
+import lecture.fastcampus.ecommerce.fpay.representation.request.payment.PaymentCancel;
+import lecture.fastcampus.ecommerce.fpay.representation.request.payment.PaymentSettlement;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Component;
 import retrofit2.Response;
 
 import java.io.IOException;
+import java.util.List;
 
 @Component
 @RequiredArgsConstructor
-public class TossPayments  {
+public class TossPayments implements PaymentAPIs {
 
     private final TossPaymentsAPIs tossClient;
 
@@ -20,6 +26,21 @@ public class TossPayments  {
             return response.body();
         }
         throw new IOException(response.message());
+    }
+
+    @Override
+    public boolean isPaymentApproved(String status) {
+        return false;
+    }
+
+    @Override
+    public ResponsePaymentCancel requestPaymentCancel(String paymentKey, PaymentCancel cancelMessage) throws IOException {
+        return null;
+    }
+
+    @Override
+    public List<ResponsePaymentSettlements> requestPaymentSettlement(PaymentSettlement paymentSettlement) throws IOException {
+        return List.of();
     }
 
 }
